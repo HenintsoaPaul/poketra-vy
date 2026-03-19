fd50c8ac783b5c71a7bf4fdec574452fc9e2c35d
henintsoapaul <henintsoapaul@gmail.com>
2026-03-19 05:28:13 +0300

    test: models

--- lib/core/models/expense.dart
+++ lib/core/models/expense.dart
@@ -63,4 +63,12 @@ class Expense {
       description: description ?? this.description,
     );
   }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Expense && runtimeType == other.runtimeType && id == other.id;
+
+  @override
+  int get hashCode => id.hashCode;
 }
